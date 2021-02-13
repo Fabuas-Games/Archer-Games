b0a2e0792ed180706ae44d2b72f0d3df244a165f
uneyss <73793862+uneyss@users.noreply.github.com>
2021-02-13 12:57:55 +0300

    Ok Atış Sistemi Eklendi

---
+++ Archer Games/Assets/ArrowScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool HasHit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HasHit == false)
+        {
+            TrackMovement();
+        }
+        
+    }
+
+    void TrackMovement()
+    {
+        Vector2 direction = rb.velocity;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        HasHit = true;
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
+    }
+}
---
+++ Archer Games/Assets/BowScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowScript : MonoBehaviour
+{
+    public Vector2 Direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 BowPos = transform.position;
+        Direction = MousePos - BowPos;
+        FaceMouse();
+    }
+
+    void FaceMouse()
+    {
+        transform.right = Direction;
+    }
+}
